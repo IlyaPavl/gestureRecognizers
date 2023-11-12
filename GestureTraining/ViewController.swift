a68e2ec88967ea1a126614afcca9c2b126d66a9c
IlyaPavl <ilyapav@icloud.com>
2023-11-12 14:37:12 +0300

    Initial commit

---
+++ GestureTraining/ViewController.swift
@@ -0,0 +1,119 @@
+//
+//  ViewController.swift
+//  GestureTraining
+//
+//  Created by Ilya Pavlov on 11.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    private let zView = UIView()
+    
+    private let viewTapGestureRecognizer = UITapGestureRecognizer()
+    
+    private let pinchGestureRecognizer = UIPinchGestureRecognizer()
+    private let zTapGestureRecognizer = UITapGestureRecognizer()
+    private let longPressGestureRecognizer = UILongPressGestureRecognizer()
+    
+    private var scale: CGFloat = 1.0 {
+        didSet {
+            scaleZView()
+        }
+    }
+    
+    private var pinchGestureAnchorScale: CGFloat?
+    
+    private var cornerRadius: CGFloat? = 0.0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.addGestureRecognizer(viewTapGestureRecognizer)
+
+        setupZView()
+        setupGestureReconizers()
+    }
+    
+}
+
+extension ViewController {
+    
+    private func setupZView() {
+        zView.frame = CGRect(x: 0, y: 0, width: 150, height: 150)
+        zView.center = view.center
+        view.addSubview(zView)
+        
+        zView.backgroundColor = .systemCyan
+        
+        zView.addGestureRecognizer(pinchGestureRecognizer)
+        zView.addGestureRecognizer(zTapGestureRecognizer)
+        zView.addGestureRecognizer(longPressGestureRecognizer)
+    }
+    
+    private func setupGestureReconizers() {
+        pinchGestureRecognizer.addTarget(self, action: #selector(handlePinchGesture(_:)))
+        zTapGestureRecognizer.addTarget(self, action: #selector(handleTapGesture(_:)))
+        longPressGestureRecognizer.addTarget(self, action: #selector(handleLongpressGesture(_:)))
+        
+        viewTapGestureRecognizer.addTarget(self, action: #selector(handleViewTapGesture(_:)))
+    }
+    
+    @objc private func handlePinchGesture(_ gestureRecognizer: UIPinchGestureRecognizer) {
+        guard pinchGestureRecognizer === gestureRecognizer else { return }
+        
+        switch gestureRecognizer.state {
+        case .began:
+            pinchGestureAnchorScale = gestureRecognizer.scale
+        case .changed:
+            guard let pinchGestureAnchorScale = pinchGestureAnchorScale else { return }
+            let gestureScale = gestureRecognizer.scale
+            
+            scale += gestureScale - pinchGestureAnchorScale
+            self.pinchGestureAnchorScale = gestureScale
+            
+        case .ended, .cancelled:
+            pinchGestureAnchorScale = nil
+        case .failed, .possible:
+            break
+        @unknown default:
+            print(" ")
+        }
+    }
+    
+    @objc private func handleTapGesture(_ gestureRecognizer: UITapGestureRecognizer) {
+        zView.backgroundColor = UIColor(red: CGFloat.random(in: 0...1), green: CGFloat.random(in: 0...1), blue: CGFloat.random(in: 0...1), alpha: 1.0)
+    }
+    
+    @objc private func handleLongpressGesture(_ gestureRecognizer: UITapGestureRecognizer) {
+        if gestureRecognizer.state == .began {
+            let newCornerRadius: CGFloat = self.zView.layer.cornerRadius > 19 ? 0 : 20
+            
+            UIView.animate(withDuration: 0.5) {
+                self.zView.layer.cornerRadius = newCornerRadius
+            }
+        }
+    }
+    
+    @objc func handleViewTapGesture(_ gestureRecognizer: UITapGestureRecognizer) {
+
+        print("view tapped")
+    }
+    
+    private func scaleZView() {
+        zView.transform = CGAffineTransform.identity.scaledBy(x: scale, y: scale)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        
+    }
+}
+
+extension ViewController: UIGestureRecognizerDelegate {
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        let severalGestures = [pinchGestureRecognizer, zTapGestureRecognizer, longPressGestureRecognizer]
+        return severalGestures.contains(gestureRecognizer) && severalGestures.contains(otherGestureRecognizer)
+    }
+}
+
